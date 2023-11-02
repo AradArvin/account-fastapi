dd0df54032bb88c15970d5731c6d9311fd4491c7
arad <aradarvinxo@gmail.com>
2023-11-02 15:13:39 +0330

    Chore: find data by another field method

--- account/db/mongodb.py
+++ account/db/mongodb.py
@@ -22,3 +22,10 @@ class MongoDBConnectionManager:
         return result
 
 
+    async def find_data_by_another_field(self, field_name: str, field_data: str):
+        result = await self.collection.find_one({f"{field_name}":field_data})
+        if result:
+            result["_id"] = str(result["_id"])
+        return result
+    
+
