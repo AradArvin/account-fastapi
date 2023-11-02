ae62c57812a3243b0dc8f6c575bde48ad99047c3
arad <aradarvinxo@gmail.com>
2023-11-02 15:13:15 +0330

    Chore: find data by id method

--- account/db/mongodb.py
+++ account/db/mongodb.py
@@ -14,3 +14,11 @@ class MongoDBConnectionManager:
         self.database = self.client[database]
         self.collection = self.database[collection]
 
+
+    async def find_data_by_id(self, instance_id: ObjectId):
+        result = await self.collection.find_one({"_id":instance_id})
+        if result:
+            result["_id"] = str(result["_id"])
+        return result
+
+
