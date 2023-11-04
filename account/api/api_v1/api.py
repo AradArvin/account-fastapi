0c9174ce7b273cdf3a2232a8a788ee7b9d3831c4
arad <aradarvinxo@gmail.com>
2023-11-04 17:20:19 +0330

    Chore: user signup route

--- account/api/api_v1/api.py
+++ account/api/api_v1/api.py
@@ -14,3 +14,21 @@ user_collection = get_collection()
 
 
 
+@account_router.post(path="/api/v1/signup", summary="User Signup", response_model=Tokens, status_code=status.HTTP_201_CREATED)
+async def user_signup(user: User = Body(), 
+                      user_service: UserService = Depends(),):
+    
+    user = jsonable_encoder(user)
+
+    if await user_collection.find_data_by_another_field(field_name="email", field_data=user["email"]):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User with this email already exists!")
+    
+    created_user = await user_service.add_user(user)
+
+    user_id = {"id":str(created_user.inserted_id)}
+    response_data = await httpx_response("api/v1/signup", user_id)
+
+    return response_data
+
+
+
