090a9fbb3f6aa4d62999e5829f69749cea3eabb1
arad <aradarvinxo@gmail.com>
2023-11-06 22:52:47 +0330

    Chore: included interactions router

--- account/main.py
+++ account/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from api.api_v1.api import account_router, data_router
+from api.api_v1.api import account_router, data_router, interactions_router
 
 
 app = FastAPI()
@@ -8,3 +8,4 @@ app = FastAPI()
 
 app.include_router(account_router, tags=["accounts"], prefix="/account")
 app.include_router(data_router, tags=["data"], prefix="/data")
+app.include_router(interactions_router, tags=["interactions"], prefix="/interactions")
