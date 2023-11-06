8ab924d7862d268c575e2e4e777b1a7190085d33
arad <aradarvinxo@gmail.com>
2023-11-06 22:54:24 +0330

    Refactor: new imports and new router

--- account/api/api_v1/api.py
+++ account/api/api_v1/api.py
@@ -1,14 +1,17 @@
 from fastapi import APIRouter, Body, status, HTTPException, Depends
 from fastapi.encoders import jsonable_encoder
 from bson.objectid import ObjectId
+from starlette.responses import JSONResponse
 
 from schema.user import *
 from service.user import *
-from connection.httpx_manager import httpx_response
+from service.otp import OTPService
+from connection.httpx_manager import httpx_response, httpx_response_otp
 
 
 account_router = APIRouter()
 data_router = APIRouter()
+interactions_router = APIRouter()
 
 user_collection = get_collection()
 
@@ -40,7 +43,7 @@ async def user_signup(user: User = Body(),
                       user_service: UserService = Depends(),):
     
     user = jsonable_encoder(user)
-
+    
     if await user_collection.find_data_by_another_field(field_name="email", field_data=user["email"]):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User with this email already exists!")
     
