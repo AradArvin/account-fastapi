912247ef8eebda2e2a97d4fe06b249f4427459f5
arad <aradarvinxo@gmail.com>
2023-11-06 22:54:47 +0330

    Chore: httpx request for otp

--- account/connection/httpx_manager.py
+++ account/connection/httpx_manager.py
@@ -4,8 +4,16 @@ from core import settings
 
 
 async def httpx_response(authorization_endpoint: str, data: dict = None):
-    print(data)
+
     async with httpx.AsyncClient() as client:
         response = await client.post(f"{settings.AUTHORIZATION_ADDRESS}/{authorization_endpoint}", json=data)
+
+    return response.json()
+
+
+async def httpx_response_otp(email_endpoint: str, data: dict = None):
+
+    async with httpx.AsyncClient() as client:
+        response = await client.post(f"{settings.EMAIL_ADDRESS}/{email_endpoint}", json=data)
 
     return response.json()
