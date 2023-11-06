1d6a6f7a6af418e2dabb4940836de74c981ce8d3
arad <aradarvinxo@gmail.com>
2023-11-06 22:55:04 +0330

    Chore: adress for redis and auth

--- account/core/settings.py
+++ account/core/settings.py
@@ -1,3 +1,7 @@
 AUTHORIZATION_ADDRESS = "http://127.0.0.1:8001/authorization"
 
+EMAIL_ADDRESS = "http://127.0.0.1:8002/email"
+
 MONGODB_HOST_ADDRESS = "mongodb://localhost:27017/"
+
+REDIS_HOST_ADDRESS = "redis://127.0.0.1:6379/1"
