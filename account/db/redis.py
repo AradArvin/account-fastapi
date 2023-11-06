3d58ec46aff23b7b8faf7ad92e30b51836f5c198
arad <aradarvinxo@gmail.com>
2023-11-06 22:55:54 +0330

    Chore: check otp method

--- account/db/redis.py
+++ account/db/redis.py
@@ -8,3 +8,12 @@ HOST_ADDRESS: str = settings.REDIS_HOST_ADDRESS
 redis = aioredis.from_url(HOST_ADDRESS, decode_responses=True)
 
 
+async def check_otp(otp: str):
+
+    all_keys = await redis.keys("*")
+
+    for key in all_keys:
+        if await redis.get(key) == otp:
+            return key.split("_")[1]
+    
+    return None
