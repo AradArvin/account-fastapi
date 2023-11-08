877be6bdaa6ad8a2b9d12ffe90ba057f428a8cd1
arad <aradarvinxo@gmail.com>
2023-11-08 22:20:55 +0330

    Chore: interactions collection

--- account/db/db.py
+++ account/db/db.py
@@ -2,7 +2,24 @@ from .mongodb import MongoDBConnectionManager
 
 user_collection = MongoDBConnectionManager(database="users", collection="user_data")
 
+like_collection = MongoDBConnectionManager(database="interaction", collection="like")
+comment_collection = MongoDBConnectionManager(database="interaction", collection="comment")
+bookmark_collection = MongoDBConnectionManager(database="interaction", collection="bookmark")
+subscribe_collection = MongoDBConnectionManager(database="interaction", collection="subscribe")
+
 
 def get_collection():
     return user_collection
 
+
+def get_interaction_collection(collection_name: str):
+    if collection_name == "like":
+        return like_collection
+    elif collection_name == "comment":
+        return comment_collection
+    elif collection_name == "bookmark":
+        return bookmark_collection
+    elif collection_name == "subscribe":
+        return subscribe_collection
+    else:
+        return None
