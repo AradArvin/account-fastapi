f73560de0c9970c246ab82cc868749252d41032c
arad <aradarvinxo@gmail.com>
2023-11-08 23:59:08 +0330

    Chore: now user_id is taken from token

--- account/service/interactions.py
+++ account/service/interactions.py
@@ -1,33 +1,44 @@
 from db.db import get_interaction_collection
 from exception.exception import *
 from connection.httpx_manager import httpx_response_with_header
+from jwt_auth.utils import token_decode
 
 
 class InteractionsService:
 
-    def __init__(self, collection_name) -> None:
-        self.collection = get_interaction_collection(collection_name)
+    def __init__(self) -> None:
+        pass
 
 
-    async def save_interaction_data(self, request, data):
+    async def save_interaction_data(self, request, data, collection_name):
 
         bearer = request.headers.get("Authorization")
         response = await httpx_response_with_header("api/v1/check-login", bearer)
 
+        token_payload = token_decode(bearer.split(" ")[1])
+        data["user_id"] = token_payload["user_id"]
+
+        collection = get_interaction_collection(collection_name)
+
         if response.get("status", None) == "logged_in":
-            save = await self.collection.save_data_to_db_collection(data)
+            save = await collection.save_data_to_db_collection(data)
             return save
         else:
             raise UserNotLoggedInError
         
 
-    async def get_interaction_data_list(self, request):
+    async def get_interaction_data_list(self, request, collection_name):
 
         bearer = request.headers.get("Authorization")
         response = await httpx_response_with_header("api/v1/check-login", bearer)
 
+        token_payload = token_decode(bearer.split(" ")[1])
+        user_id = token_payload["user_id"]
+
+        collection = get_interaction_collection(collection_name)
+
         if response.get("status", None) == "logged_in":
-            data_list = await self.collection.get_data_from_db_collection()
+            data_list = await collection.get_data_by_query("user_id", user_id)
             return data_list
         else:
             raise UserNotLoggedInError
