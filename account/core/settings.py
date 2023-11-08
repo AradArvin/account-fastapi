b4105411c99aa85f13cd5538c4c0c22ff49076ed
arad <aradarvinxo@gmail.com>
2023-11-08 22:20:36 +0330

    Chore: jwt token settings

--- account/core/settings.py
+++ account/core/settings.py
@@ -1,3 +1,12 @@
+from decouple import config
+
+
+
+
+JWT_SECRET = config("JWT_SECRET_KEY")
+JWT_ALGORITHM = config("JWT_ALGORITHM")
+
+
 AUTHORIZATION_ADDRESS = "http://127.0.0.1:8001/authorization"
 
 EMAIL_ADDRESS = "http://127.0.0.1:8002/email"
@@ -5,3 +14,4 @@ EMAIL_ADDRESS = "http://127.0.0.1:8002/email"
 MONGODB_HOST_ADDRESS = "mongodb://localhost:27017/"
 
 REDIS_HOST_ADDRESS = "redis://127.0.0.1:6379/1"
+
