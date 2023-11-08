b0422afb2d9d1a63039e143df26d327d4f83f9fa
arad <aradarvinxo@gmail.com>
2023-11-08 22:20:06 +0330

    Chore: httpx request with header and data

--- account/connection/httpx_manager.py
+++ account/connection/httpx_manager.py
@@ -3,7 +3,7 @@ from core import settings
 
 
 
-async def httpx_response(authorization_endpoint: str, data: dict = None):
+async def httpx_response(authorization_endpoint: str, data: dict):
 
     async with httpx.AsyncClient() as client:
         response = await client.post(f"{settings.AUTHORIZATION_ADDRESS}/{authorization_endpoint}", json=data)
@@ -22,6 +22,18 @@ async def httpx_response_with_header(authorization_endpoint: str, auth_token: st
 
 
 
+async def httpx_response_with_header_and_data(authorization_endpoint: str, data: dict, auth_token: str = None):
+
+    headers = {"Authorization": auth_token}
+    async with httpx.AsyncClient(headers=headers) as client:
+        response = await client.put(f"{settings.AUTHORIZATION_ADDRESS}/{authorization_endpoint}", json=data)
+
+    return response.json()
+
+
+
+
+async def httpx_response_otp(email_endpoint: str, data: dict):
 
     async with httpx.AsyncClient() as client:
         response = await client.post(f"{settings.EMAIL_ADDRESS}/{email_endpoint}", json=data)
