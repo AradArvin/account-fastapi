03690cfbb47227e8e5c796c5a3ddcbae927fa1b2
arad <aradarvinxo@gmail.com>
2023-11-08 22:21:14 +0330

    Chore: check login exception

--- account/exception/exception.py
+++ account/exception/exception.py
@@ -14,3 +14,9 @@ class NoDataEnteredError(Error):
     
     def __init__(self, message: str = "No data entered!") -> None:
         super().__init__(message)
+
+
+class UserNotLoggedInError(Error):
+    
+    def __init__(self, message: str = "User not logged in!") -> None:
+        super().__init__(message)
