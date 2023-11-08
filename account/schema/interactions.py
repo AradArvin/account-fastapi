6022e25e5c742b4af2ee664df4adcbc164d88c04
arad <aradarvinxo@gmail.com>
2023-11-08 23:59:24 +0330

    Chore: new response models

--- account/schema/interactions.py
+++ account/schema/interactions.py
@@ -4,24 +4,47 @@ from pydantic import Field, BaseModel
 class Like(BaseModel):
     _id: str
     episode_id: str = Field()
-    user_id: str = Field()
+
+
+
+class LikeResponse(BaseModel):
+    episode_id: str
+    user_id: str
 
 
 class Comment(BaseModel):
     _id: str
     episode_id: str = Field()
-    user_id: str = Field()
     content: str = Field()
+
+
+class CommentResponse(BaseModel):
+    episode_id: str
+    user_id: str
+    content: str
+
 
 
 class Bookmark(BaseModel):
     _id: str
     episode_id: str = Field()
-    user_id: str = Field()
     bookmark_list_name: str | None=None
+
+
+class BookmarkResponse(BaseModel):
+
+    episode_id: str
+    user_id: str
+    bookmark_list_name: str | None=None
+
 
 
 class Subscribe(BaseModel):
     _id: str
     rss_id: str = Field()
-    user_id: str = Field()
+
+
+
+class SubscribeResponse(BaseModel):
+    rss_id: str
+    user_id: str
